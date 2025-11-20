4a544f5aa40e348884b7b98b4035c4af99067a95
maćkozaw <116316918+mackozaw@users.noreply.github.com>
2025-11-20 16:16:04 +0100

    add detection module

--- NotesRecognitionModule/train.py
+++
@@ -1,6 +0,0 @@
-import torch
-from ultralytics import YOLO
-
-model = YOLO('yolov8m.pt')
-device = torch.device("cuda:1")
-results = model.train(data="deep_scores.yaml", imgsz=640, rect=True, epochs = 10, batch=4)
